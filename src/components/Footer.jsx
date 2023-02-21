f18059c87d0543ee3cba289663556f7e8d26fc17
Madebybrown <111353481+Madebybrown@users.noreply.github.com>
2023-02-21 13:39:45 +0100

    Update copyright text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {AiOutlineCopyrightCircle} from 'react-icons/ai'
 
 export default function Footer() {
   return (
     <div className="flex justify-center items-center">
-      <p className="text-sm text-gray-600 dark:text-white">
-        2023 copyright | all rights reserved.
+      <p className="text-sm text-gray-600 dark:text-white flex justify-center items-center gap-1">
+        2023 copyright <AiOutlineCopyrightCircle /> Madebybrown | all rights reserved.
       </p>
     </div>
   );
