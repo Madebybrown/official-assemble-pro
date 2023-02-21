cfc73fe3f34413356f878a0abcc6e02bcf5abeff
Madebybrown <111353481+Madebybrown@users.noreply.github.com>
2023-02-21 13:34:28 +0100

    Add global margin and padding, add grid layout to product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Product = () => {
   return (
     <div className="p-8 flex justify-center items-center h-full w-full">
-      <div>
-        <h2>Products Coming Soon..</h2>
+      <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
+        <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">Articles</div>
+        <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">Video</div>
+        <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">Checklist</div>
+        <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">Instructions</div>
       </div>
     </div>
   );
