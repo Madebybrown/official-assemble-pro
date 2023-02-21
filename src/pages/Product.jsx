09f20bc1596732faaa5fe8ac1dfed1a3a5d805de
Madebybrown <111353481+Madebybrown@users.noreply.github.com>
2023-02-21 14:16:57 +0100

    Add minor styling to product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -14,9 +14,10 @@ const Product = () => {
     <div className="p-8 flex justify-center items-center h-full w-full">
       <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
         {image.articles && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">
-            <h2>Articles</h2>
-            <ul>
+          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center">
+            <h2 className="font-bold uppercase">Articles</h2>
+            <p>|</p>
+            <ul className="flex gap-3">
               {image.articles.split(", ").map((article, index) => (
                 <li key={index}>{article}</li>
               ))}
@@ -24,9 +25,10 @@ const Product = () => {
           </div>
         )}
         {image.video && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">
-            <h2>Video</h2>
-            <ul>
+          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center">
+            <h2 className="font-bold uppercase">Video</h2>
+            <p>|</p>
+            <ul className="flex gap-3">
               {image.video.split(", ").map((video, index) => (
                 <li key={index}>{video}</li>
               ))}
@@ -34,9 +36,10 @@ const Product = () => {
           </div>
         )}
         {image.checklist && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">
-            <h2>Checklist</h2>
-            <ul>
+          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center">
+            <h2 className="font-bold uppercase">Checklist</h2>
+            <p>|</p>
+            <ul className="flex gap-3">
               {image.checklist.split(", ").map((checklist, index) => (
                 <li key={index}>{checklist}</li>
               ))}
@@ -44,9 +47,10 @@ const Product = () => {
           </div>
         )}
         {image.instructions && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex justify-center items-center">
-            <h2>Instructions</h2>
-            <ul>
+          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center">
+            <h2 className="font-bold uppercase">Instructions</h2>
+            <p>|</p>
+            <ul className="flex gap-3">
               {image.instructions.split(", ").map((instruction, index) => (
                 <li key={index}>{instruction}</li>
               ))}
