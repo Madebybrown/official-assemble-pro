d3b528781af42a25e0b011e1991d89d5c9dd1510
Madebybrown <111353481+Madebybrown@users.noreply.github.com>
2023-03-14 10:29:36 +0100

    Update dataset, add ability to iterate through Articles and Video in ProductView and update divs accordingly

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -10,85 +10,169 @@ const images = [
     id: "1",
     src: Cables,
     alt: "Cables",
-    articles: {
-      artnum: "60124-465",
-      artname: "mc6s 4x10",
-      amount: "4",
-      location: "62-01-04",
-    },
-    video: "test 4, test 5, test 6", // Connected to imported video
+    articles: [
+      {
+        artnum: "60124-465",
+        artname: "mc6s 4x10",
+        amount: "4",
+        location: "62-01-04",
+      },
+      {
+        artnum: "70124-777",
+        artname: "nfi6s 2x10",
+        amount: "88",
+        location: "66-03-09",
+      },
+    ],
+    video: [
+      {
+        op: "Test 1",
+      },
+      {
+        op: "Test 2",
+      },
+    ],
     checklist: "test 7, test 8, test 9",
-    instructions: "test 11, test 12, test 13",
   },
   {
     id: "2",
     src: Laser,
     alt: "Laser",
-    articles: {
-      artnum: "34324-888",
-      artname: "Front Panel",
-      amount: "1",
-      location: "62-03-03",
-    },
-    video: "test 17, test 18, test 19",
+    articles: [
+      {
+        artnum: "34324-888",
+        artname: "Front Panel",
+        amount: "1",
+        location: "62-03-03",
+      },
+      {
+        artnum: "89462-888",
+        artname: "back Panel",
+        amount: "3",
+        location: "62-03-03",
+      },
+    ],
+    video: [
+      {
+        op: "Test 3",
+      },
+      {
+        op: "Test 4",
+      },
+    ],
     checklist: "test 20, test 21, test 22",
-    instructions: "test 23, test 24, test 25",
   },
   {
     id: "3",
     src: Machinery,
     alt: "Machinery",
-    articles: {
-      artnum: "60121-565",
-      artname: "mc6s 3x20",
-      amount: "2",
-      location: "62-05-04",
-    },
-    video: "test 29, test 30, test 31",
+    articles: [
+      {
+        artnum: "92744-338",
+        artname: "dbwu wuw",
+        amount: "1",
+        location: "62-03-03",
+      },
+      {
+        artnum: "89462-888",
+        artname: "top Panel",
+        amount: "2",
+        location: "62-03-03",
+      },
+    ],
+    video: [
+      {
+        op: "Test 5",
+      },
+      {
+        op: "Test 6",
+      },
+    ],
     checklist: "test 32, test 33, test 34",
-    instructions: "test 35, test 36, test 37",
   },
   {
     id: "4",
     src: MotorColor,
     alt: "MotorColor",
-    articles: {
-      artnum: "88124-465",
-      artname: "Lock King m4",
-      amount: "4",
-      location: "62-01-04",
-    },
-    video: "test  41, test  42, test  43",
+    articles: [
+      {
+        artnum: "88124-465",
+        artname: "Lock King m4",
+        amount: "4",
+        location: "62-01-04",
+      },
+      {
+        artnum: "82714-465",
+        artname: "Master m55",
+        amount: "8",
+        location: "62-01-04",
+      },
+    ],
+    video: [
+      {
+        op: "Test 7",
+      },
+      {
+        op: "Test 8",
+      },
+    ],
     checklist: "test  44, test  45, test  46",
-    instructions: "test  47, test  48, test  49",
   },
   {
     id: "5",
     src: Printing,
     alt: "Printing",
-    articles: {
-      artnum: "33123-465",
-      artname: "Spacer m4",
-      amount: "1",
-      location: "62-01-04",
-    },
-    video: "test 53, test 54, test 55",
+    articles: [
+      {
+        artnum: "33123-465",
+        artname: "Spacer m4",
+        amount: "1",
+        location: "62-01-04",
+      },
+      {
+        artnum: "33123-465",
+        artname: "Skrew m4",
+        amount: "5",
+        location: "62-01-04",
+      },
+    ],
+    video: [
+      {
+        op: "Test 9",
+      },
+      {
+        op: "Test 10",
+      },
+    ],
     checklist: "test 56, test 57, test 58",
-    instructions: "test 59, test 60, test 61",
   },
   {
     id: "6",
     src: Rework,
     alt: "Rework",
-    articles: {
-      artnum: "55362-372",
-      artname: "Base Plate",
-      amount: "1",
-      location: "62-01-04",
-    },
-    video: "test 65, test 66, test 67",
+    articles: [
+      {
+        artnum: "88124-465",
+        artname: "Lock King m4",
+        amount: "4",
+        location: "62-01-04",
+      },
+      {
+        artnum: "82714-465",
+        artname: "Master m55",
+        amount: "8",
+        location: "62-01-04",
+      },
+    ],
+    video: [
+      {
+        op: "Test 11",
+      },
+      {
+        op: "Test 12",
+      },
+    ],
     checklist: "test 68, test 69, test 70",
-    instructions: "test 71, test 72, test 73",
   },
 ];
 
--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import images from "./Data";
 import { motion } from "framer-motion";
@@ -7,6 +7,9 @@ function ProductView() {
   // Get the ID of the product from the URL using the useParams hook
   const { id } = useParams();
 
+  const [currentArticleIndex, setCurrentArticleIndex] = useState(0);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
   // Find the product with the specified ID in the images array
   const image = images.find((image) => image.id === id);
 
@@ -14,6 +17,17 @@ function ProductView() {
   if (!image) {
     return <p>Image not found</p>;
   }
+
+  const handleNextArticle = () => {
+    // If there are no more articles, start over from the beginning
+    if (currentArticleIndex === image.articles.length - 1) {
+      setCurrentArticleIndex(0);
+    } else {
+      setCurrentArticleIndex(currentArticleIndex + 1);
+    }
+  };
+
+  const currentArticle = image.articles[currentArticleIndex];
 
   return (
     <motion.div
@@ -25,68 +39,75 @@ function ProductView() {
       <div className="grid grid-cols-2 grid-rows-2 w-full h-full gap-4">
         {/* If the product has articles, render a section for them */}
         {image.articles && (
-          <div className="bg-gray-300 dark:bg-gray-900 w-full p-4 flex uppercase font-semibold justify-center items-start rounded-xl">
-            {/* Render a list of the articles props */}
+          <div className="bg-gray-300 col-span-1 row-span-1 dark:bg-gray-900 w-full p-4 flex uppercase font-semibold justify-center items-start rounded-xl">
+            {/* Render a list of the current article */}
             <ul className="flex w-full justify-between md:text-xs">
               <li>
                 <span className="font-bold">Art.Num</span> <br />{" "}
                 <span className="text-blue-600 text-xs">
-                  {image.articles.artnum}
+                  {currentArticle.artnum}
                 </span>
               </li>
               <li>
                 <span className="font-bold">Art.Name</span> <br />{" "}
                 <span className="text-xs capitalize">
-                  {image.articles.artname}
+                  {currentArticle.artname}
                 </span>
               </li>
               <li>
                 <span className="font-bold">Amount</span> <br />{" "}
-                <span className="text-xs">{image.articles.amount}</span>
+                <span className="text-xs">{currentArticle.amount}</span>
               </li>
               <li>
                 <span className="font-bold">Location</span> <br />{" "}
-                <span className="text-xs">{image.articles.location}</span>
+                <span className="text-xs">{currentArticle.location}</span>
               </li>
             </ul>
+            <div className="h-full flex items-end">
+              <button
+                onClick={() => {
+                  setCurrentArticleIndex(
+                    (currentArticleIndex + 1) % image.articles.length
+                  );
+                  setCurrentVideoIndex((currentVideoIndex + 1) % image.video.length);
+                }}
+              >
+                Next
+              </button>
+            </div>
           </div>
         )}
 
         {/* If the product has videos, render a section for them */}
         {image.video && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center rounded-xl">
+          <div className="bg-gray-300 col-span-1 row-span-2 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center rounded-xl">
             <h2 className="font-bold uppercase">Video</h2>
             <p>|</p>
             {/* Render a list of the videos */}
-            <ul className="flex gap-3">
-              {image.video.split(", ").map((video, index) => (
-                <li key={index}>{video}</li>
+            <ul>
+              {image.video.map((video, index) => (
+                <li
+                  key={index}
+                  style={{
+                    display: index === currentVideoIndex ? "block" : "none",
+                  }}
+                >
+                  {video.op}
+                </li>
               ))}
             </ul>
           </div>
         )}
+
         {/* If the product has a checklist, render a section for it */}
         {image.checklist && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center rounded-xl">
+          <div className="bg-gray-300 col-span-1 row-span-1 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center rounded-xl">
             <h2 className="font-bold uppercase">Checklist</h2>
             <p>|</p>
             {/* Render a list of the checklist items */}
             <ul className="flex gap-3">
               {image.checklist.split(", ").map((checklist, index) => (
                 <li key={index}>{checklist}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {/* If the product has instructions, render a section for them */}
-        {image.instructions && (
-          <div className="bg-gray-300 dark:bg-gray-900 p-4 flex uppercase gap-3 font-semibold justify-center items-center rounded-xl">
-            <h2 className="font-bold uppercase">Instructions</h2>
-            <p>|</p>
-            {/* Render a list of the instructions */}
-            <ul className="flex gap-3">
-              {image.instructions.split(", ").map((instruction, index) => (
-                <li key={index}>{instruction}</li>
               ))}
             </ul>
           </div>
